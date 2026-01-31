d53f0bdd6c7d155005e0030138ff307da316ecd8
personfurkan <86071971+personfurkan@users.noreply.github.com>
2026-01-31 15:02:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,19 @@
-const photos = [
-  { src: "images/1.jpg", text: "Birinci fotoğraf açıklaması" },
-  { src: "images/2.jpg", text: "İkinci fotoğraf açıklaması" },
-  { src: "images/3.jpg", text: "Üçüncü fotoğraf açıklaması" },
-  // ...
-  { src: "images/30.jpg", text: "Otuzuncu fotoğraf açıklaması" }
-];
+const totalImages = 30;
+let current = 1;
 
-let index = 0;
-const img = document.getElementById("photo");
+const photo = document.getElementById("photo");
 const caption = document.getElementById("caption");
 
-function showPhoto(i) {
-  img.src = photos[i].src;
-  caption.innerText = photos[i].text;
-}
-
-document.getElementById("next").onclick = () => {
-  index = (index + 1) % photos.length;
-  showPhoto(index);
-};
-
-document.getElementById("prev").onclick = () => {
-  index = (index - 1 + photos.length) % photos.length;
-  showPhoto(index);
-};
-
-/* Mobil swipe */
-let startX = 0;
-
-img.addEventListener("touchstart", e => {
-  startX = e.touches[0].clientX;
+document.getElementById("next").addEventListener("click", () => {
+  current++;
+  if (current > totalImages) current = 1;
+  photo.src = `images/${current}.jpg`;
+  caption.textContent = `Fotoğraf ${current}`;
 });
 
-img.addEventListener("touchend", e => {
-  let endX = e.changedTouches[0].clientX;
-  if (startX - endX > 50) index++;
-  else if (endX - startX > 50) index--;
-  index = (index + photos.length) % photos.length;
-  showPhoto(index);
+document.getElementById("prev").addEventListener("click", () => {
+  current--;
+  if (current < 1) current = totalImages;
+  photo.src = `images/${current}.jpg`;
+  caption.textContent = `Fotoğraf ${current}`;
 });
-
-showPhoto(index);
